b757eaf2a30fe8724fb2ec7da263de1b0a76c32d
Manh Tung <143700010+MnhTng@users.noreply.github.com>
2025-06-01 13:56:58 +0700

    v1.0

--- _lib/auth.ts
+++ _lib/auth.ts
@@ -35,8 +35,8 @@ const providers: Provider[] = [
     Google,
     Github,
     // Google({
-    //     clientId: process.env.GOOGLE_CLIENT_ID || "",
-    //     clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    //     clientId: process.env.AUTH_GOOGLE_ID || "",
+    //     clientSecret: process.env.AUTH_GOOGLE_SECRET || "",
     //     authorization: {
     //         params: {
     //             prompt: "consent",
